fcb33ce0741e3eb03502a90effbed1bf60f82e9d
Elijah Balogun <elijah.balogun@iisysgroup.com>
2022-10-14 23:17:46 +0100

    feat: Print alphabt, no 'e' or 'q'

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		if (c == 'e' || c == 'q')
+		{
+			c++;
+			continue;
+		}
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
