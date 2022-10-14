2919dfebc1b172bacab761f0f85192345091f8b7
Elijah Balogun <elijah.balogun@iisysgroup.com>
2022-10-14 23:50:27 +0100

    feat: Print number with other characters

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int c = 48;
+
+	while (c <= 57)
+	{
+		putchar(c);
+		if (c != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
