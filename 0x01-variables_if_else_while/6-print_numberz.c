8a84ec33c1e2c9ad8c302b7a35ebb82ba79b5d7a
Elijah Balogun <elijah.balogun@iisysgroup.com>
2022-10-14 23:32:39 +0100

    feat: Print number

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int c = 48;
+
+	while (c <= 57)
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
