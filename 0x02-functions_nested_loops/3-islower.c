c6200be45d0a553e98fb82d8cfbeac1494419681
Elijah Balogun <elijah.balogun@iisysgroup.com>
2022-10-18 11:03:34 +0100

    feat: Check lowercase letters

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _islower - Check if character is lowercase
+ * @c: The character to check
+ *
+ * Return: 1 if character is lowercase, 0 if otherwise
+ */
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
