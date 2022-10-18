dbe99ab06a4b24bc150142419e4d5999aba09546
Elijah Balogun <elijah.balogun@iisysgroup.com>
2022-10-18 10:33:26 +0100

    feat: Print alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print lowercase alphabet
+ *
+ */
+void print_alphabet(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+}
