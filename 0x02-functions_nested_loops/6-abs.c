0f732e02931e1dd783cf0c4ee05c3c18f22e010d
Elijah Balogun <elijah.balogun@iisysgroup.com>
2022-10-18 11:45:55 +0100

    feat: Implement absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - Return absolute value of number
+ * @n: The number passed
+ *
+ * Return: absolute value of number passed
+ */
+int _abs(int n)
+{
+	if (n < 0)
+		return (-n);
+
+	return (n);
+}
