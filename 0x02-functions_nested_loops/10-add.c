dd3b3b36aeb2ab23166f395b606dad064b24d89c
Elijah Balogun <elijah.balogun@iisysgroup.com>
2022-10-18 17:34:09 +0100

    feat: Compute addition

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - Compute the addition of two integers
+ * @a: Integer
+ * @b: Integer
+ *
+ * Return: Addition of two numbers
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
