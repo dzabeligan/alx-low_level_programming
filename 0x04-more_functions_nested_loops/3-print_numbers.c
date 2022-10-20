bb51c0be895d962ee22c87b6a39200d57208ce4e
Elijah Balogun <elijah.balogun@iisysgroup.com>
2022-10-20 07:38:52 +0100

    feat: Print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - print numbers, from '0' to '9'
+ *
+ */
+void print_numbers(void)
+{
+	char c = '0';
+
+	while (c <= '9')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+}
