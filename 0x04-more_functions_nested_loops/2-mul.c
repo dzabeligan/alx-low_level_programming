2be377c1283e9000d12db891fdea5cccdd8baa4d
Elijah Balogun <elijah.balogun@iisysgroup.com>
2022-10-20 07:34:01 +0100

    feat: Compute product of two integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - Compute the product of two integers
+ * @a: Integer
+ * @b: Integer
+ *
+ * Return: Product of two numbers
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
