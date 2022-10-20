a8f6ddff0e8a64f513ae3a3f4311f8993076197d
Elijah Balogun <elijah.balogun@iisysgroup.com>
2022-10-20 07:29:32 +0100

    feat: Check if character is a digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isdigit - Check if character is digit
+ * @c: The character to check
+ *
+ * Return: 1 if character is digit, 0 if otherwise
+ */
+int _isdigit(int c)
+{
+	return (c >= '0' && c <= '9');
+}
