0274cb9c7670fb617a9c5108fc56944d743643a9
Elijah Balogun <elijah.balogun@iisysgroup.com>
2022-10-20 07:14:17 +0100

    feat: Check if character is uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isupper - Check if character is uppercase
+ * @c: The character to check
+ *
+ * Return: 1 if character is uppercase, 0 if otherwise
+ */
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
