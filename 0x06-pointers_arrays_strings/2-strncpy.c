ba4c87fc04c2b55a9d1fe82a2c99b8a72eb5bcfb
Elijah Balogun <elijah.balogun@iisysgroup.com>
2022-10-27 11:05:11 +0100

    feat: Implement strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strncpy - copy strings
+ * @dest: destination, string
+ * @src: source, string
+ * @n: number of characters to copy from source
+ *
+ * Return: Pointer to destination after concatenation
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (i < n && *src != '\0')
+		dest[i++] = *src++;
+	while (i < n)
+		dest[i++] = '\0';
+
+	return (dest);
+}
