dd56889a48b95aac5f2efc640f3e2e15cd82a4aa
Elijah Balogun <elijah.balogun@iisysgroup.com>
2022-10-27 10:49:22 +0100

    feat: Implement strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenate strings
+ * @dest: destination, string
+ * @src: source, string
+ * @n: number of characters to copy from source
+ *
+ * Return: Pointer to destination after concatenation
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	char *dest_copy = dest;
+
+	while (*(dest_copy++ + 1) != '\0')
+		;
+	while (n-- && (*dest_copy++ = *src++) != '\0')
+		;
+	return (dest);
+}
