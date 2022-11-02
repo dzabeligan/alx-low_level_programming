d34a0cc403a37188d28231822f571f726977e55c
Elijah Balogun <elijah.balogun@iisysgroup.com>
2022-11-02 21:23:05 +0100

    feat: Implement _pow_recursion

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - Compute the value of x raised to the power of y
+ * @x: number
+ * @y: power
+ *
+ * Return: value of x raised to the power of y
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+	return (x * _pow_recursion(x, y - 1));
+}
