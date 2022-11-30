cb7b37520d60b55b189788484d090236b7743e62
Elijah Balogun <elijah.balogun@iisysgroup.com>
2022-11-30 02:54:48 +0100

    feat: Implement find loop

---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,39 @@
+#include "lists.h"
+
+/**
+ * find_listint_loop - find loop in list
+ * @head: list
+ *
+ * Return: NULL if there is no loop, address of the node otherwise
+ */
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *tortoise, *hare;
+
+	if (head == NULL || head->next == NULL)
+		return (NULL);
+
+	tortoise = head->next;
+	hare = head->next->next;
+
+	while (hare)
+	{
+		if (tortoise == hare)
+		{
+			tortoise = head;
+
+			while (tortoise != hare)
+			{
+				tortoise = tortoise->next;
+				hare = hare->next;
+			}
+
+			return (tortoise);
+		}
+
+		tortoise = tortoise->next;
+		hare = hare->next->next;
+	}
+
+	return (NULL);
+}
